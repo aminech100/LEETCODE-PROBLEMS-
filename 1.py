a383e7d519d22afb9d9b71567a8c0428735b3736
aminech100 <chrifamine2006@gmail.com>
2025-03-09 20:22:10 +0000

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dct ={}
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in dct and i != dict[diff]:
-                return [dct[diff],i]
-            dct[nums[i]] = i
