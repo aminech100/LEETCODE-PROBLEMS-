160a6cd42c3b648b735d163a9b286feab4dd25d1
aminech100 <chrifamine2006@gmail.com>
2025-03-13 20:19:29 +0000

    Time: 9 ms (21.12%), Space: 17.9 MB (21.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,7 +1,5 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        class Solution:
-    def romanToInt(self, s: str) -> int:
         dct = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         result = 0
         i = 0
